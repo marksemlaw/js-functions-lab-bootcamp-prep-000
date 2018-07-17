99667c82930aea41a1512a63d02d68460f13d42b
Mark Semotiuk <marksem@gmail.com>
2018-07-17 15:26:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ function happyHolidays (String) {
 }
 
 function happyHolidaysTo (name) {
-  return 'Happy holidays, ${name}!'
+  return "Happy holidays, ${name}!"
 }
 
 function happyHolidayTo (holiday, name) {
-  return 'Happy ${holiday}, ${name}!'
+  return "Happy ${holiday}, ${name}!"
 }
 
 function holidayCountdown (holiday, days) {
