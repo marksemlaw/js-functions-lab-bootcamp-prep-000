4b8171072a7e370b0368f569195ddadd3d0a363e
Mark Semotiuk <marksem@gmail.com>
2018-07-17 14:25:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+
+function happyHolidays (String) {
+  return("Happy holidays!")
+}
+
+function happyHolidaysTo (String) {
+  return('Happy holidays!', ${name}!)
+}
+
+function happyHolidayTo (String) {
+  return("Happy holidays!")
+}
+
+function holidayCountdown (String) {
+  return("Happy holidays!")
+}
