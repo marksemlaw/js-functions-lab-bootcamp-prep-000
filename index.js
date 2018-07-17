7ad3fddebbf16fc78d55706ce1460aeff00a51b8
Mark Semotiuk <marksem@gmail.com>
2018-07-17 14:40:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function happyHolidays (String) {
 }
 
 function happyHolidaysTo (name) {
-  return('Happy holidays, ',
-  '${name}',
-  '!')
+  return 'Happy holidays, '
+  '${name}'
+  '!'
 }
 
 function happyHolidayTo (holiday, name) {
